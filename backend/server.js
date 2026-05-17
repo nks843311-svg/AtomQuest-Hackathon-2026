7edd8356b2471e03440da3e763b9b89933eadcea
nks843311-svg <nks843311@gmail.com>
2026-05-17 21:14:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "goalportal"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err)=>{
