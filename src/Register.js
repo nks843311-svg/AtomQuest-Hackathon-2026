6a3dc7ce97a1cdf80d83d9aa4221c094cb051fff
nks843311-svg <nks843311@gmail.com>
2026-05-18 15:49:04 +0530

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Register.css"; // Make sure to add your styling here
+
 
 function Register() {
     const navigate = useNavigate();
